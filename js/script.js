0ca9e0e6b3eec658b5f65665ad3250a9204f483f
Evanildo Silva <evanildodasilva.dev@gmail.com>
2022-03-07 18:51:06 +0100

    quarto commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const botaoMobile = document.getElementById('menuMobile');
 
-function ativarMenu(){
+function ativarMenu(event){
+    if(event.type==='touchstart') event.preventDefault();
+    
     const nav = document.getElementById('nav');  
     nav.classList.toggle('active')
 
 }
 
 botaoMobile.addEventListener('click', ativarMenu);
+botaoMobile.addEventListener('touchstart', ativarMenu);
